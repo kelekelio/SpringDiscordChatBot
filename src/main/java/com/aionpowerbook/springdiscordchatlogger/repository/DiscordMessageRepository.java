9063aee75c40809b5c1cb9ffe366560ce46941ee
Grzegorz Nowakowski <cristal01@gmail.com>
2022-10-14 14:28:09 +0200

    Specification and models

--- src/main/java/com/aionpowerbook/springdiscordchatlogger/repository/DiscordMessageRepository.java
+++ src/main/java/com/aionpowerbook/springdiscordchatlogger/repository/DiscordMessageRepository.java
@@ -2,6 +2,7 @@ package com.aionpowerbook.springdiscordchatlogger.repository;
 
 import com.aionpowerbook.springdiscordchatlogger.entity.DiscordMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface DiscordMessageRepository extends JpaRepository<DiscordMessage, Long> {
+public interface DiscordMessageRepository extends JpaRepository<DiscordMessage, Long>, JpaSpecificationExecutor<DiscordMessage> {
 }
